fee3d514c86cbb76313abfc1198d3eb11d2f68a5
ArtemZ <Art1me00@mail.ru>
2025-10-29 23:21:51 +0700

    make managers for CustomUser

--- Tomsk_kedr/api/models.py
+++ Tomsk_kedr/api/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
-
+from api.managers import CustomBaseManager
 #Создание класса пользователя с использованием AbstractUser
-class CustomUsers(AbstractUser):
+class User(AbstractUser):
     phone_number = PhoneNumberField('Телефон',unique=True)
     birth_data = models.DateField('Дата рождения')
     place_of_work = models.CharField('Место работы', max_length=100)
     
     #USERNAME_FIELD = ''
     #REQUIRED_FIELDS = ''
+    
+    objects = CustomBaseManager()
+    
     
     class Meta:
         verbose_name = 'Польззователь'
