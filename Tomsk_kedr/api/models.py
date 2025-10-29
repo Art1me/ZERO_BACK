39358ca0f368b36a4cdfab1d83ce8e2dcbb3b0ec
ArtemZ <Art1me00@mail.ru>
2025-10-29 22:15:05 +0700

    create CastomUser but don't migrate

--- Tomsk_kedr/api/models.py
+++ Tomsk_kedr/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+#Создание класса пользователя с использованием AbstractUser
+class CastomUsers(AbstractUser):
+    phone_number = PhoneNumberField('Телефон',unique=True)
+    birth_data = models.DateField('Дата рождения')
+    place_of_work = models.CharField('Место работы', max_length=100)
+    
+    #USERNAME_FIELD = ''
+    #REQUIRED_FIELDS = ''
+    
+    class Meta:
+        verbose_name = 'Польззователь'
+        verbose_name_plural = 'Пользователи'
