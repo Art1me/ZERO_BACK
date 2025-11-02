84cde03d9d9ac4e5c45c449a1069dfef2bf03c3e
ArtemZ <Art1me00@mail.ru>
2025-11-02 10:34:51 +0700

    Custom the admin panel for creat and change the users, also you can filter the users

--- Tomsk_kedr/api/admin.py
+++ Tomsk_kedr/api/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from api.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    change_user_password_template = None
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'email', 'username', 'is_corporate_account')}),
+        (_('Личная информация'),
+         {'fields': ('first_name', 'last_name','place_of_work',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone_number','first_name','last_name','birthday', 'password1', 'password2',),
+        }),
+    )
+    list_display = ('id', 'full_name', 'email', 'phone_number','is_corporate_account', 'is_active', 'is_staff', 'is_superuser', )
+
+    list_display_links = ('id', 'full_name',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('first_name', 'last_name', 'id', 'email', 'phone_number',)
+    ordering = ('-id',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    readonly_fields = ('last_login',)
+
+    
--- Tomsk_kedr/api/models.py
+++ Tomsk_kedr/api/models.py
@@ -4,9 +4,28 @@ from phonenumber_field.modelfields import PhoneNumberField
 from api.managers import CustomBaseManager
 #Создание класса пользователя с использованием AbstractUser
 class User(AbstractUser):
-    phone_number = PhoneNumberField('Телефон',unique=True)
-    birth_data = models.DateField('Дата рождения')
-    place_of_work = models.CharField('Место работы', max_length=100)
+    #поля для обязательных данных
+    username = models.CharField('Имя пользователя', unique=True, max_length=150, null = True, )
+    phone_number = PhoneNumberField('Телефон',unique=True, null = True)
+    email = models.EmailField('Электронная почта', unique=True, null = True) 
+    password = models.CharField('Пароль', max_length=128)
+    first_name = models.CharField('Имя', max_length=100,null = True)
+    last_name = models.CharField('Фамилия', max_length=100,null = True)
+    birthday = models.DateField('Дата рождения', null=True, blank=True)
+    
+    #поля для дополнительных данных
+    place_of_work = models.CharField('Место работы', max_length=100,null = True)
+    
+    #поля для проверки подтверждения аккаунта и корпоративного аккаунта
+    is_verified = models.BooleanField('Подтвержден', default=True)
+    is_corporate_account = models.BooleanField('Корпоративный аккаунт', default=False)
+    
+    
+    #поля для администратора
+    is_active = models.BooleanField('Активен', default=True)
+    is_superuser = models.BooleanField('Суперпользователь', default=False)
+    is_staff = models.BooleanField('Staff', default=False)
+    
     
     #USERNAME_FIELD = ''
     #REQUIRED_FIELDS = ''
@@ -15,5 +34,14 @@ class User(AbstractUser):
     
     
     class Meta:
-        verbose_name = 'Польззователь'
+        verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def __str__(self):
+        return f'{self.full_name} ({self.pk})'
+    
+    
